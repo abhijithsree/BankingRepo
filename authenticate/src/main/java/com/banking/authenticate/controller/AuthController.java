a97226e0368d34929439e1ba1eecd3d8ffcedfd1
abhi <abhi@host.docker.internal>
2021-01-01 14:20:01 +0530

    [get method added]

--- authenticate/src/main/java/com/banking/authenticate/controller/AuthController.java
+++ authenticate/src/main/java/com/banking/authenticate/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.banking.authenticate.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -7,9 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,25 @@ public class AuthController {
 	@ResponseBody
 	public ResponseEntity<BankingResponse> validateUser(@RequestBody Map<String,Object> request){
 		logger.info("Auth flow is calling");
+		boolean valid = authService.validateUser(request);
+		logger.info("valid user",valid);
+		BankingResponse response = new BankingResponse();
+		response.setMessage("success");
+		response.setStatusCode(HttpStatus.OK.value());
+		response.setData(valid);
+		ResponseEntity<BankingResponse> bankingresponse = new ResponseEntity<BankingResponse> (response,HttpStatus.OK);
+		return bankingresponse;
+		
+	}
+	
+	@GetMapping("/loginget")
+	@ResponseBody
+	public ResponseEntity<BankingResponse> validateUser(@RequestParam("username") String userName,@RequestParam("password") String password){
+		logger.info("Auth flow is calling");
+		Map<String,Object> request = new HashMap<String,Object>();
+		request.put("name", userName);
+		request.put("password", password);
+
 		boolean valid = authService.validateUser(request);
 		logger.info("valid user",valid);
 		BankingResponse response = new BankingResponse();
