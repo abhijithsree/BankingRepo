56a1bd97752a99a110612e59c55a4fb60ddb017a
abhi <abhi@DESKTOP-7ONJOVS>
2021-01-02 23:41:22 +0530

    [commends]

--- accountbalance/src/test/java/com/banking/accountbalance/accountbalance/AccountbalanceApplicationTests.java
+++ accountbalance/src/test/java/com/banking/accountbalance/accountbalance/AccountbalanceApplicationTests.java
@@ -1,13 +1,12 @@
-package com.banking.accountbalance.accountbalance;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AccountbalanceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.banking.accountbalance.accountbalance;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class AccountbalanceApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
